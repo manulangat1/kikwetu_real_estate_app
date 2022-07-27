28097d6045158a552c33cb0ad356013265e0e426
manulangat <emmanuelthedeveloper@gmail.com>
2022-07-27 17:45:31 +0300

    chore:splitting settings file

---
+++ real_estate/settings/development.py
@@ -0,0 +1,11 @@
+from .base import *
+DATABASES = {
+    'default': {
+        'ENGINE': env("POSTGRES_ENGINE"),
+        'NAME': env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
